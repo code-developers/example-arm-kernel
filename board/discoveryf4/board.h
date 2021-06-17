92dbd36805ad3b7c8db70c8a5f2b810936efc1c0
krishpranav <krisna.pranav@gmail.com>
2021-06-17 12:03:04 +0530

    [+] includes [=]

--- board/discoveryf4/board.h
+++ board/discoveryf4/board.h
@@ -1,2 +1,8 @@
 #ifndef DISCOVERYF4_BOARD_H_
 #define DISCOVERYF4_BOARD_H_
+
+#include <platform/stm32f4/registers.h>
+#include <platform/stm32f4/gpio.h>
+#include <platform/stm32f4/usart.h>
+#include <platform/stm32f4/nvic.h>
+#include <platform/stm32f4/systick.h>
