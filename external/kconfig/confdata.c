2d15f0a341b1f7d3c237d2667321db4b47bc8f96
krishpranav <krisna.pranav@gmail.com>
2021-06-17 14:52:22 +0530

    [+] includes [+]

---
+++ external/kconfig/confdata.c
@@ -0,0 +1,11 @@
+// includes
+#include <sys/stat.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <libgen.h>
