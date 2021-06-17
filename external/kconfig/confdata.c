8417cdb6ccdf947dd356436700561145998fe198
krishpranav <krisna.pranav@gmail.com>
2021-06-17 14:55:06 +0530

    [+] conf warning [+]

--- external/kconfig/confdata.c
+++ external/kconfig/confdata.c
@@ -23,3 +23,14 @@ static const char *conf_filename;
 static int conf_lineno, conf_warning, conf_unsaved;
 
 const char conf_defname[] = ".defconfig";
+
+static void conf_warning(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	fprintf(stderr, "%s:%d:warning: ", conf_filename, conf_lineno);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	va_end(ap);
+	conf_warnings++;
+}
