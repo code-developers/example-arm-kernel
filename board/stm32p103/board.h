9c21798b6d2f72ec15286dc8b4d67456eb12eaf3
krishpranav <krisna.pranav@gmail.com>
2021-06-17 12:36:17 +0530

    [+] includes [+]

--- board/stm32p103/board.h
+++ board/stm32p103/board.h
@@ -1,2 +1,8 @@
 #ifndef STM32P103_BOARD_H_
 #define STM32P103_BOARD_H_
+
+#include <platform/stm32f1/registers.h>
+#include <platform/stm32f1/gpio.h>
+#include <platform/stm32f1/usart.h>
+#include <platform/stm32f1/nvic.h>
+#include <platform/stm32f1/systick.h>
