fb6041be7faac654c72a1ec6f89c15d8c6d9768e
krishpranav <krisna.pranav@gmail.com>
2021-06-17 12:03:50 +0530

    [=] usart, config dgb port [+]

--- board/discoveryf4/board.h
+++ board/discoveryf4/board.h
@@ -6,3 +6,7 @@
 #include <platform/stm32f4/usart.h>
 #include <platform/stm32f4/nvic.h>
 #include <platform/stm32f4/systick.h>
+
+extern struct usart_dev console_uart;
+
+#if defined(CONFIG_DBGPORT_USE_USART1)
