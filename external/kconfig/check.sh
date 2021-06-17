5b8d5b9be8b6761b897f348d648fbdeff1a871bf
krishpranav <krisna.pranav@gmail.com>
2021-06-17 12:46:57 +0530

    [+] dkbuild check [+]

---
+++ external/kconfig/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+$* -xc -o /dev/null - > /dev/null 2>&1 << EOF
+#include <libintl.h>
+int main()
+{
+	gettext("");
+	return 0;
+}
+EOF
+if [ ! "$?" -eq "0"  ]; then
+	echo -DKBUILD_NO_NLS;
+fi
