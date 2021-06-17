57e43962319c652101b2b60adfe9b1b338832f89
krishpranav <krisna.pranav@gmail.com>
2021-06-17 12:08:58 +0530

    [+] includes [+]

--- board/discoveryf429/board.h
+++ board/discoveryf429/board.h
@@ -1,2 +1,8 @@
 #ifndef DISCOVERYF429_BOARD_H_
 #define DISCOVERYF429_BOARD_H_
+
+#include <platform/stm32f429/registers.h>
+#include <platform/stm32f429/gpio.h>
+#include <platform/stm32f429/usart.h>
+#include <platform/stm32f429/nvic.h>
+#include <platform/stm32f429/systick.h>
