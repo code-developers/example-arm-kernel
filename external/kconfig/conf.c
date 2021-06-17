0858a3c3784345e609d69c858889cfe2fbcee269
krishpranav <krisna.pranav@gmail.com>
2021-06-17 13:25:39 +0530

    [+] includes [+]

---
+++ external/kconfig/conf.c
@@ -0,0 +1,11 @@
+// includes
+#include <locale.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <sys/stat.h>
+#include <sys/time.h>
