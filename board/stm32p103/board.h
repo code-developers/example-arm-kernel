7b1f26e1d4b6f6942011b6168db227295810ce22
krishpranav <krisna.pranav@gmail.com>
2021-06-17 12:36:58 +0530

    [+] usart [+]

--- board/stm32p103/board.h
+++ board/stm32p103/board.h
@@ -6,3 +6,7 @@
 #include <platform/stm32f1/usart.h>
 #include <platform/stm32f1/nvic.h>
 #include <platform/stm32f1/systick.h>
+
+extern struct usart_dev console_uart;
+
+#if defined(CONFIG_DBGPORT_USE_USART1)
